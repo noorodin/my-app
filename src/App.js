238bef61cbf80dc1e93b99734828d71663736201
noorodin <n.kiani@outlook.com>
2023-09-15 09:45:06 -0500

    some changes..

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>some changes.</p>
+        <p>some changes..</p>
       </header>
     </div>
   );
